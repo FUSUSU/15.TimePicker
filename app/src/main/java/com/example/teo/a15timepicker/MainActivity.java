082fea7af82371e6c992bc12a129c7af13a7ad40
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-09 18:20:29 +0700

    Code hoàn thành

--- app/src/main/java/com/example/teo/a15timepicker/MainActivity.java
+++ app/src/main/java/com/example/teo/a15timepicker/MainActivity.java
@@ -3,13 +3,44 @@ package com.example.teo.a15timepicker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    TimePicker timePicker1;
+    Button changetime1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ánh xạ
+        textView1 = (TextView) findViewById(R.id.textView1);
+        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+        //Cài đặt hiển thị chế độ 24h
+        timePicker1.setIs24HourView(true);
+        changetime1 = (Button) findViewById(R.id.button1);
+
+        //Hiển thị thời gian hiện tại ra textView1
+        textView1.setText(getCurrentTime());
+
+        changetime1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView1.setText(getCurrentTime());
+            }
+        });
+    }
+
+    //Hàm lấy thời gian từ TimePicker
+    public String getCurrentTime() {
+        String currentTime = "Current Time: "+timePicker1.getCurrentHour()+":"+timePicker1.getCurrentMinute();
+        return currentTime;
     }
 
     //Menu item
